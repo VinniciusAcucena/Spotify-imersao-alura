8c27f9339d471dbfd88ced72610348855b5e339d
Vinnicius Açucena <90075631+VinniciusAcucena@users.noreply.github.com>
2024-01-26 13:57:54 -0300

    Dia 5 da Imersão

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
+const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
@@ -10,25 +10,25 @@ function requestApi(searchTerm) {
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add('hidden');
+    resultPlaylist.classList.add("hidden")
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
     result.forEach(element => {
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
-    })
+    });
 
     resultArtist.classList.remove('hidden');
 }
 
-document.addEventListener('input', () => {
+document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
-    if(searchTerm === '') {
+    if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
-        return;
+        return
     }
-
+    
     requestApi(searchTerm);
 })
